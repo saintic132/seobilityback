da86cf6505a2eb260bfd0aec1834b52b1f239ea5
saintic <saintic132@gmail.com>
2022-06-27 18:44:57 +0300

    Сервер под тест

---
+++ index.js
@@ -0,0 +1,31 @@
+import express from 'express'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+
+const PORT = 5000;
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+app.use(bodyParser.json())
+
+const validate = (req, res, next) => {
+    const {name, email, phone, birthday, message} = req.body;
+    if (!(name && email && phone && birthday && message)) {
+        res.status(400).json({
+            status: 'error',
+            message: 'First and Last name, email, phone, birthday and message cannot be empty'
+        })
+    }
+    next()
+}
+
+app.post('/', validate, (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        message: 'Success data send'
+    });
+});
+
+app.listen(PORT, () => console.log('SERVER START AT PORT ' + PORT))
