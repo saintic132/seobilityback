5a0c776cc35e3fe4770bd74d8b7975ff824e5142
saintic <saintic132@gmail.com>
2022-06-27 19:44:59 +0300

    commit1

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import bodyParser from 'body-parser'
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5005;
 const app = express()
 
 app.use(express.json())
